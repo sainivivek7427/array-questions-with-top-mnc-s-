5c7ca96974f147754a295e3df24d236e3d5aeb91
sainivivek7427 <93831086+sainivivek7427@users.noreply.github.com>
2022-03-03 12:52:08 +0530

    Update maximum_circular_subarray_sum.cpp

--- maximum_circular_subarray_sum.cpp
+++ maximum_circular_subarray_sum.cpp
@@ -25,6 +25,16 @@ int main(){
 		cout<<a[i]<<"  ";
 	}
 	cout<<endl;
+	// to check all negative number  to return -1
+    int checksum=0;
+    for (int i = 0; i < n; ++i)
+	{
+		checksum+=a[i];
+	}
+    if(checksum<0){
+        cout<<"\n all the values are negative the not go any line ";
+        return 0;
+    }
     int totalsum=0;
     //multiply -1 of total array and sum of total array 
     for (int i = 0; i < n; ++i)
